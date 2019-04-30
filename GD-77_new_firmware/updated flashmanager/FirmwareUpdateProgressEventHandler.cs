4da4787916d8c55683866f5c182eaf648174c712
Kai Ludwig <email@dg4klu.de>
2019-04-30 08:50:35 +0200

    cleanup

--- GD-77_new_firmware/updated flashmanager/FirmwareUpdateProgressEventHandler.cs
+++
@@ -1,4 +0,0 @@
-namespace GD77_FlashManager
-{
-	public delegate void FirmwareUpdateProgressEventHandler(object sender, FirmwareUpdateProgressEventArgs e);
-}
--- GD-77_new_firmware/updated flashmanager/IFirmwareUpdate.cs
+++
@@ -1,8 +0,0 @@
-namespace GD77_FlashManager
-{
-	public interface IFirmwareUpdate
-	{
-		event FirmwareUpdateProgressEventHandler OnFirmwareUpdateProgress;
-
-	}
-}
