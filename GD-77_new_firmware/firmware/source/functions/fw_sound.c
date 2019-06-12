ef0613ca25667862e431dbae9dd5a86878e3e23b
Kai Ludwig <email@dg4klu.de>
2019-06-12 14:17:55 +0200

    Optimized audio buffer processing to only use the left channel of the I2C DAC.

--- GD-77_new_firmware/firmware/source/functions/fw_sound.c
+++ GD-77_new_firmware/firmware/source/functions/fw_sound.c
@@ -253,7 +253,8 @@ void store_soundbuffer()
 		taskEXIT_CRITICAL();
 	}
 }
-
+#define LINEOUT2
+//#define LINEOUT1
 void send_sound_data()
 {
 	if (wavbuffer_count>0)
@@ -263,28 +264,44 @@ void send_sound_data()
 			switch(g_SAI_TX_Handle.queueUser)
 			{
 			case 0:
+#ifdef LINEOUT2
 				spi_sound1[4*i+3]=wavbuffer[wavbuffer_read_idx][2*i+1];
 				spi_sound1[4*i+2]=wavbuffer[wavbuffer_read_idx][2*i];
+#endif
+#ifdef LINEOUT1
 				spi_sound1[4*i+1]=wavbuffer[wavbuffer_read_idx][2*i+1];
 				spi_sound1[4*i]=wavbuffer[wavbuffer_read_idx][2*i];
+#endif
 				break;
 			case 1:
+#ifdef LINEOUT2
 				spi_sound2[4*i+3]=wavbuffer[wavbuffer_read_idx][2*i+1];
 				spi_sound2[4*i+2]=wavbuffer[wavbuffer_read_idx][2*i];
+#endif
+#ifdef LINEOUT1
 				spi_sound2[4*i+1]=wavbuffer[wavbuffer_read_idx][2*i+1];
 				spi_sound2[4*i]=wavbuffer[wavbuffer_read_idx][2*i];
+#endif
 				break;
 			case 2:
+#ifdef LINEOUT2
 				spi_sound3[4*i+3]=wavbuffer[wavbuffer_read_idx][2*i+1];
 				spi_sound3[4*i+2]=wavbuffer[wavbuffer_read_idx][2*i];
+#endif
+#ifdef LINEOUT1
 				spi_sound3[4*i+1]=wavbuffer[wavbuffer_read_idx][2*i+1];
 				spi_sound3[4*i]=wavbuffer[wavbuffer_read_idx][2*i];
+#endif
 				break;
 			case 3:
+#ifdef LINEOUT2
 				spi_sound4[4*i+3]=wavbuffer[wavbuffer_read_idx][2*i+1];
 				spi_sound4[4*i+2]=wavbuffer[wavbuffer_read_idx][2*i];
+#endif
+#ifdef LINEOUT1
 				spi_sound4[4*i+1]=wavbuffer[wavbuffer_read_idx][2*i+1];
 				spi_sound4[4*i]=wavbuffer[wavbuffer_read_idx][2*i];
+#endif
 				break;
 			}
 		}
