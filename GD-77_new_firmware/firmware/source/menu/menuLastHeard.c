21826ae6f2f0835c1fadd7660ba439983399f873
Kai Ludwig <email@dg4klu.de>
2019-06-13 08:37:37 +0200

    cleanup

--- GD-77_new_firmware/firmware/source/menu/menuLastHeard.c
+++ GD-77_new_firmware/firmware/source/menu/menuLastHeard.c
@@ -62,7 +62,6 @@ static void updateScreen()
 	int numDisplayed=0;
 	LinkItem_t *item = LinkHead;
 
-
 	UC1701_clearBuf();
 	UC1701_printCentered(0, "Last heard",UC1701_FONT_GD77_8x16);
 
@@ -73,7 +72,6 @@ static void updateScreen()
 	}
 	while((item != NULL) && item->id != 0)
 	{
-
 		dmrIDLookup(item->id,&foundRecord);
 		{
 			UC1701_printCentered(16+(numDisplayed*16), foundRecord.text,UC1701_FONT_GD77_8x16);
