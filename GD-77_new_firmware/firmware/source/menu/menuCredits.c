d7a3e821e6816c6ce436b07783756d2c65a55d3b
Roger Clark <roger@rogerclark.net>
2019-07-04 16:48:59 +1000

    Updated credits

--- GD-77_new_firmware/firmware/source/menu/menuCredits.c
+++ GD-77_new_firmware/firmware/source/menu/menuCredits.c
@@ -31,7 +31,7 @@ static void scrollDownOneLine();
 
 #define CREDIT_TEXT_LENGTH 33
 static const int NUM_LINES_PER_SCREEN = 7;
-static const char creditTexts[][CREDIT_TEXT_LENGTH] = {"Conceived & developed","by","Kai DG4KLU","with help from","Roger VK3KYY","....","DMR Vocoder binary","from","Radioddity's","official firmware",""};
+static const char creditTexts[][CREDIT_TEXT_LENGTH] = {"Conceived & developed","by","Kai DG4KLU","with help from","Roger VK3KYY"};
 static int currentDisplayIndex=0;
 
 int menuCredits(int buttons, int keys, int events, bool isFirstRun)
